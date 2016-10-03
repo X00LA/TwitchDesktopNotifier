8ff1480b2af79a763b555fae8f15c9b535dd300e
CFeldpa2 <Feldpc15@gmail.com>
2016-10-02 22:38:51 -0400

    Initial commit

---
+++ src/portfolio/feldpc/TwitchNotifier/MainDriver.java
@@ -0,0 +1,15 @@
+/**
+ * This program sends a notification to the desktop whenever a new channel goes live from the users following list
+ * 
+ * @author Collin Feldpausch
+ * @version 0.1
+ */
+package portfolio.feldpc.TwitchNotifier;
+
+public class MainDriver {
+
+	public static void main(String[] args) {
+				
+	}
+
+}
